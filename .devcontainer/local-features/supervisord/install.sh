1e7dc0ff9b7af77183da13cbe0a80e68417b695c
Tuomas Pöyry <me@tuomas.id>
2025-05-14 17:24:00 +0300

    Add Supervisord

---
+++ .devcontainer/local-features/supervisord/install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eax
+
+RUN apt-get update \
+  && apt-get -y supervisor
+
+tee -a /etc/supervisord.conf >&- <<EOT
+[supervisord]
+nodaemon=true
+user=root
+logfile=/dev/null
+logfile_maxbytes=0
+pidfile=/var/run/supervisord.pid
+
+[unix_http_server]
+file=/var/run/supervisor.sock
+
+[supervisorctl]
+serverurl=unix:///var/run/supervisor.sock
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[program:code-server]
+directory=/app
+user=codespace
+environment=HOME="/home/codespace"
+command=/usr/local/bin/code-server-entrypoint
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+EOT
