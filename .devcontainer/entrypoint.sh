32a6ea97047d2635d08e92bd4f444b3e75302847
Tuomas <me@tuomas.id>
2025-05-15 12:51:04 +0300

    Try out overrideCommand

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch /test.asd
+
 # Detect if we are in an interactive shell
 if [[ -t 1 && "$#" -eq 1 && "$1" == "bash" ]]; then
     echo "Switching to non-root user for interactive shell"
